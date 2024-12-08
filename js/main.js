9dbbf91cc9020faa2a96f9cd56fff4dc9bfe9645
Naresh Battulwar <naresh.battulwar@definelabs.com>
2024-12-08 17:06:40 +0530

    push-5

--- js/main.js
+++ js/main.js
@@ -64,6 +64,11 @@ $(document).ready(function(){
     $('.filter-box').removeClass('show-filter');
   })
 
+  $('.price-range-btn').click(function(){
+    $(this).toggleClass('active-price-range-btn')
+    $('.price-range').toggleClass('show-price-range');
+  })
+
 })
 
 
@@ -210,6 +215,35 @@ $(document).ready(function(){
 })
 
 
+
+$('#searchModal').on('shown.bs.modal', function (e) {
+  $('.popular-cities').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    arrows: true,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+})
+
+
 function showExtraLi(_plotType){
   $(_plotType).toggleClass('show-extra-li');
   $(_plotType+'-show-more').toggleClass('active-show-more');
@@ -220,3 +254,47 @@ function showExtraLi(_plotType){
   
 //   $(_btnSel).addClass('testssssss')
 // }
+
+
+const priceInputs = document.querySelectorAll(".price-input input");
+const rangeInputs = document.querySelectorAll(".range-input input");
+const range = document.querySelector(".slider .progress");
+
+let priceGap = 1000;
+
+priceInputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minPrice = parseInt(priceInputs[0].value);
+    let maxPrice = parseInt(priceInputs[1].value);
+
+    if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInputs[1].max) {
+      if (e.target.className === "min-input") {
+        rangeInputs[0].value = minPrice;
+        range.style.left = (minPrice / rangeInputs[0].max) * 100 + "%";
+      } else {
+        rangeInputs[1].value = maxPrice;
+        range.style.right = 100 - (maxPrice / rangeInputs[1].max) * 100 + "%";
+      }
+    }
+  });
+});
+
+rangeInputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minVal = parseInt(rangeInputs[0].value);
+    let maxVal = parseInt(rangeInputs[1].value);
+
+    if (maxVal - minVal < priceGap) {
+      if (e.target.className === "min-range") {
+        rangeInputs[0].value = maxVal - priceGap;
+      } else {
+        rangeInputs[1].value = minVal + priceGap;
+      }
+    } else {
+      priceInputs[0].value = minVal;
+      priceInputs[1].value = maxVal;
+      range.style.left = (minVal / rangeInputs[0].max) * 100 + "%";
+      range.style.right = 100 - (maxVal / rangeInputs[1].max) * 100 + "%";
+    }
+  });
+});
